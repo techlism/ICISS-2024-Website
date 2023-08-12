18052beeeaaa955d813e74763879aec0bef2aab9
techlism <105585386+techlism@users.noreply.github.com>
2023-08-12 12:09:47 +0530

    fixed date component bug

--- src/components/TimeLeftCounter.jsx
+++ src/components/TimeLeftCounter.jsx
@@ -1,19 +1,15 @@
 import React, { useEffect, useState } from 'react';
 
-const TimeLeftCounter = () => {
-  const targetDate = new Date('December 20, 2024 00:00:00');
+const TimeLeftCounter = () => {  
   const [daysLeft, setDaysLeft] = useState(0);
 
   useEffect(() => {
-    const interval = setInterval(() => {
+      const targetDate = new Date('December 20, 2024 00:00:00');
       const currentDate = new Date();
       const timeDifference = targetDate.getTime() - currentDate.getTime();
       const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
       setDaysLeft(days);
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, [targetDate]);
+  },[setDaysLeft]);
 
   return (
     <div style={{paddingTop:'15px'}}>

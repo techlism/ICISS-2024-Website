0562cef0758e7ef565f08476533c527ee0b28979
Kundan <2021itb049.kundan@students.iiests.ac.in>
2024-04-07 21:23:13 +0530

    added speakers to the speakers page

--- src/components/pages/SpeakersPage.jsx
+++ src/components/pages/SpeakersPage.jsx
@@ -1,15 +1,41 @@
 // import ProfessorCard from "../../ProfessorCard";
 // import profile from '../../assets/profile.jpg'
+
 import styling from './SpeakersPage.module.css'
 const SpeakersPage = ()=>{
-    return(
+    return (
         <div className={styling['main-speakerpage']}>
             <div className={styling.professors}>
                 <h1 className={styling.heading}>Our Speakers</h1>
-                <p className={styling['para']}>To be announced..</p>                       
+                <div style={{ display: 'flex', flexWrap: 'wrap' , justifyContent : 'center', color : '#EDEEF7'}}>
+                    <div style={{ width: '25%', padding: '10px' , border : 'solid 2px black', borderRadius : '8px', marginRight : '5px'}}>
+                        <img
+                            src="/speaker01.jpg"
+                            alt="Debdeep Mukhopadhyay Image"
+                            style={{ borderRadius: '8px', width: '100%' }}
+                        />
+                        <h1>Debdeep Mukhopadhyay</h1>
+                        <p>
+                            Professor, Computer Science and Engineering, Indian Institute of
+                            Technology, Kharagpur, India
+                        </p>
+                    </div>
+                    <div style={{ width: '25%', padding: '10px' ,border : 'solid 2px black', borderRadius : '8px'}}>
+                        <img
+                            src="/speaker02.png"
+                            alt="Sansanee Auephanwiriyakul Image"
+                            style={{ borderRadius: '8px', width: '100%' }}
+                        />
+                        <h1>Sansanee Auephanwiriyakul</h1>
+                        <p>
+                            Associate Professor, Computer Engineering Department, Chiang Mai
+                            University, Chiangmai, Thailand
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default SpeakersPage;

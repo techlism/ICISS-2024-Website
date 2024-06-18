a3a170450aeda3f0b4f41cbd8b556c062e0b9a43
techlism <2021itb049.kundan@students.iiests.ac.in>
2024-06-18 20:53:05 +0530

    added springer publishing paper name

--- src/components/pages/ContributionTrackPage.jsx
+++ src/components/pages/ContributionTrackPage.jsx
@@ -12,6 +12,16 @@ const ContributionTrackPage = ()=>{
             }}>
                 Note : All the accepted papers will be published through a <img src={springerLogo} alt="springer" style={{backgroundColor:'#edeef7',backgroundClip:'content-box',borderRadius:'8px'}}/> series (Waiting for approval).
             </p>
+            <p
+                style={{
+                    'textAlign':'center',
+                    'fontSize':'1.5rem',
+                    fontWeight:'500',
+                    color:'#EDEEF7',
+                }}
+            >
+                Best papers (1st, 2nd and 3rd ) in each track of the conference may be considered for publication in <strong>Journal of The Institution of Engineers (India): Series B (SCImago and SCOPUS indexed)</strong>  with appropriate enhancement (up to 80%) and suitable peer review process of the Journal.
+            </p>            
         </div>
     )
 }
--- src/components/ui/About.jsx
+++ src/components/ui/About.jsx
@@ -29,6 +29,9 @@ const About = ()=>{
             <p>
                 Note : All the accepted papers will be published through a <img src={springerLogo} alt="springer" style={{backgroundColor:'#edeef7',backgroundClip:'content-box',borderRadius:'8px'}}/> series (Waiting for approval).
             </p>
+            <p>
+            Best papers (1st, 2nd and 3rd ) in each track of the conference may be considered for publication in <strong>Journal of The Institution of Engineers (India): Series B (SCImago and SCOPUS indexed)</strong>  with appropriate enhancement (up to 80%) and suitable peer review process of the Journal.
+            </p>
     </div>
  )    
 }

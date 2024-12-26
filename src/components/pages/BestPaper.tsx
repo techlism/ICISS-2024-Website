62ca39f8274f0bc6e5f6bfefd3434cc465e1d4a0
techlism <2021itb049.kundan@students.iiests.ac.in>
2024-12-26 13:41:38 +0530

    added best paper

--- src/components/pages/BestPaper.tsx
+++ src/components/pages/BestPaper.tsx
@@ -1,11 +1,12 @@
 import { Worker, Viewer } from '@react-pdf-viewer/core';
 import '@react-pdf-viewer/core/lib/styles/index.css';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+import React from 'react';
 
 export default function BestPaperPage() {
     return (
             <Worker workerUrl={"https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js"}>
-                <Viewer fileUrl="/schedule.pdf" />
+                <Viewer fileUrl="/best_paper.pdf" />
             </Worker>
     );
 }
